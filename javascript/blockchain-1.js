b5aa0f53a1ba020f295b25f4c4c62819ea26e5ec
Yusuf Emre Katkat <emre.katkat@std.yildiz.edu.tr>
2025-11-01 17:36:05 +0300

    Update blockchainfile.js
    Fix blockchain hash calculation and validation logic

--- javascript/blockchain-1.js
+++ javascript/blockchain-1.js
@@ -18,31 +18,34 @@ class Block {
   // crypto modülünü ve SHA256 kullanarak hash oluşturun.
   calculateHash() {
     return crypto.createHash("sha256")
-      .update(/* buraya blok verilerini string olarak ekle */)
+      .update(this.index + this.previousHash + this.timestamp + JSON.strinigfy(this.data))
       .digest("hex");
   }
-}
 
 // 2. Blockchain sınıfını tanımla
-class Blockchain {
-  constructor() {
+  class Blockchain {
+   constructor() {
     // Blok zincirini başlatırken ilk blok oluşturulur
     this.chain = [this.createGenesisBlock()];
   }
 
   // İlk blok (Genesis Block)
   createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis Block", "0");
     // Yeni bir Block nesnesi döndürün.
     // index: 0, data: "Genesis Block", previousHash: "0"
   }
 
   // Son bloğu döndür
   getLatestBlock() {
-    return /* zincirin son bloğunu döndür */
+    return this.chain[this.chain.length - 1]; /* zincirin son bloğunu döndür */
   }
 
   // 3. Yeni blok ekleme fonksiyonu
   addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
     // newBlock.previousHash değerini güncelleyin (son bloğun hash’i)
     // newBlock.hash değerini yeniden hesaplayın
     // zincire ekleyin
@@ -51,6 +54,13 @@ class Blockchain {
 
   // Zinciri doğrulama fonksiyonu
   isChainValid() {
+    for (let i=1; i<this.chain.length; i++) {
+      const current = this.chain[i];
+      const previous = this.chain[i-1];
+      if (current.hash !== current.calculateHash()) return false
+      if (current.previousHash !== previous.hash) return false
+    }
+    return true;
     // Tüm blokları kontrol edin:
     // Hash’ler doğru mu?
     // previousHash bir önceki bloğa eşit mi?
@@ -76,3 +86,5 @@ console.log("\nChain geçerli mi?", myChain.isChainValid());
 // Zinciri bozmayı deneyin (isteğe bağlı)
 // myChain.chain[1].data = { amount: 9999, from: "Hacker", to: "Kendisi" };
 // console.log("Chain geçerli mi?", myChain.isChainValid());
+myChain.chain[1].data = { amount: 999, from: "Hacker", to: "Me" };
+console.log("Chain geçerli mi?", myChaib.isChainValid());
